df0d81466d5b7b3984d11e8094522841f85c372c
turgutmuslu <72387188+turgutmuslu@users.noreply.github.com>
2021-03-11 01:26:49 +0300

    Update eylemyapamiyomus.cpp

--- eylemyapamiyomus.cpp
+++ eylemyapamiyomus.cpp
@@ -9,13 +9,13 @@ int main()
 
 	cout << endl << "Here is your choice: " << number;
 
-	cout << "Your number is divisible by";
+	cout << "\n\n" << "Your number is divisible by";
 
 	if (number % 2 == 0) cout << " 2";
-	else if (number % 3 == 0) cout << " 3";
-	else if (number % 5 == 0) cout << " 5";
-	else if (number % 7 == 0) cout << " 7";
-	else cout << "none of them..";
+	if (number % 3 == 0) cout << " 3";
+	if (number % 5 == 0) cout << " 5";
+	if (number % 7 == 0) cout << " 7";
+	if (number % 2 != 0 and number % 3 != 0 and number % 5 != 0 and number % 7 != 0) cout << " none of them..";
 
 	cout << endl;
 }
